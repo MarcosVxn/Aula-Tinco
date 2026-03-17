5d19b8dfe11a41f68b5146edc0137bd3bded673d
Marcos <marcosjoaomj76@gmail.com>
2026-03-16 22:50:42 -0300

    Correção Atividade

--- Python/CalculoKWh.py
+++ Python/CalculoKWh.py
@@ -3,3 +3,17 @@ print('1 - Residencia')
 print('2 - Indústria')
 print('3 - comércio')
 tipoInstalação = int(input('Qual tipo de intalação: '))
+
+if (tipoInstalação == 1 and kwhReceber <= 500):
+    res = kwhReceber * 0.40
+    print('valor a pagar {}'.format(res))
+elif(tipoInstalação == 1 and kwhReceber > 500):
+    res = kwhReceber * 0.65
+    print('valor a pagar {}'.format(res))
+    
+if (tipoInstalação == 2 and kwhReceber <= 1000):
+    res = kwhReceber * 0.55
+    print('valor a pagar {}'.format(res))
+elif (tipoInstalação == 2 and kwhReceber >1000):
+    res = kwhReceber * 0.60
+    print('valor a pagar {}'.format(res))
